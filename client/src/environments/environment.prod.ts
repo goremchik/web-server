b7a16ea8a02c51321f7783b8f105213d95791b67
Andrii Shoferivskyi <Andrii_Shoferivskyi@epam.com>
2021-07-14 09:42:31 +0300

    t

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
+const a = '';
 export const environment = {
   production: true,
 
   apiUrls: {
     quote: '/api',
-    share: 'https://pd56uwt1gi.execute-api.eu-central-1.amazonaws.com/dev',
+    share: '',
   },
 };
